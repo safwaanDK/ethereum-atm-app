5f985fea0cc86ef2ac0c960fa9bc9e3aea894d0e
Safwaan Uddin <safwaanuddin@gmail.com>
2023-07-27 02:16:30 +0530

    Add existing file

---
+++ contracts/Assessment.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        balance += _amount;
+
+        assert(balance == _previousBalance + _amount);
+
+        emit Deposit(_amount);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        balance -= _withdrawAmount;
+
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        emit Withdraw(_withdrawAmount);
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == owner, "You are not the owner of this contract");
+        require(newOwner != address(0), "Invalid new owner address");
+
+        emit OwnershipTransferred(owner, newOwner);
+        owner = payable(newOwner);
+    }
+}
+
---
+++ pages/index.js
@@ -0,0 +1,146 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [owner, setOwner] = useState(undefined);
+  const [newOwnerAddress, setNewOwnerAddress] = useState("");
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const atmABI = atm_abi.abi;
+
+  const getWallet = async () => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const account = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(account);
+    }
+  };
+
+  const handleAccount = (account) => {
+    if (account) {
+      console.log("Account connected: ", account);
+      setAccount(account);
+    } else {
+      console.log("No account found");
+    }
+  };
+
+  const connectAccount = async () => {
+    if (!ethWallet) {
+      alert("MetaMask wallet is required to connect");
+      return;
+    }
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
+    handleAccount(accounts);
+
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
+    if (atm) {
+      setBalance((await atm.getBalance()).toNumber());
+    }
+  };
+
+  const getOwner = async () => {
+    if (atm) {
+      const contractOwner = await atm.getOwner();
+      setOwner(contractOwner);
+    }
+  };
+
+  const transferOwnership = async () => {
+    if (atm && newOwnerAddress !== "") {
+      await atm.transferOwnership(newOwnerAddress);
+      getOwner();
+      setNewOwnerAddress(""); // Reset the input field after transferring ownership
+    }
+  };
+
+  const deposit = async () => {
+    if (atm) {
+      let tx = await atm.deposit(1);
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(1);
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>Please install Metamask in order to use this ATM.</p>;
+    }
+
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>;
+    }
+
+    if (balance === undefined) {
+      getBalance();
+    }
+
+    if (owner === undefined) {
+      getOwner();
+    }
+
+    return (
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance}</p>
+        <p>Contract Owner: {owner}</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <div>
+          <label>New Owner Address: </label>
+          <input type="text" value={newOwnerAddress} onChange={(e) => setNewOwnerAddress(e.target.value)} />
+          <button onClick={transferOwnership}>Transfer Ownership</button>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return (
+    <main className="container">
+      <header>
+        <h1>Welcome to the Metacrafters ATM!</h1>
+      </header>
+      {initUser()}
+      <style jsx>{`
+        .container {
+          text-align: center;
+        }
+      `}</style>
+    </main>
+  );
+}
